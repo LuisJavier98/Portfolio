fc6d9f611210cc29d6835eecb09adbb7f0df7718
Luis <luisjavier_2705@hotmail.com>
2022-12-23 16:15:27 -0500

    implementacion de particulas

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,39 @@ import './App.css'
 import video from '../src/Video/Smoke.mp4'
 import image from '../src/Images/Foto.png'
 import CV from '../src/CV/CV_Luis_Burga.pdf'
-import { BsInstagram } from 'react-icons/bs'
-import { BsGithub } from 'react-icons/bs'
-import { AiOutlineLinkedin } from 'react-icons/ai'
-import { AiOutlineCopyright } from 'react-icons/ai'
-import { useRef } from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import Projects from '../src/Pojects'
-import { skills } from '../src/Pojects'
-import { AiOutlineMenu } from 'react-icons/ai'
+import { BsGithub, BsDownload, BsInstagram } from 'react-icons/bs'
+import { AiOutlineLinkedin, AiOutlineCopyright, AiOutlineMenu } from 'react-icons/ai'
+import { useRef, useState, useEffect, useCallback } from 'react'
+import Projects, { skills } from '../src/Pojects'
 import { IoIosCloseCircleOutline } from 'react-icons/io'
-import { BsDownload } from 'react-icons/bs'
 import Typewriter from 'typewriter-effect'
 import AOS from 'aos';
 import 'aos/dist/aos.css'
-
+import Particles from 'react-particles'
+import { loadFull } from 'tsparticles'
 
 function App() {
   const [projectNumber, setprojectNumber] = useState(0)
   const [render, setrender] = useState()
   const [isActive, setisActive] = useState(false)
+  const [language, setlanguage] = useState("english")
   const handleChange = () => setisActive(!isActive)
   const ProjectMove = useRef()
   const SelectCard = useRef()
   const Reference = useRef()
   const Header = useRef()
 
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+  }, []);
+
+
   let height = window.innerHeight
-
-
   window.onscroll = function () {
     if (scrollY > (height - 10)) {
       Header.current.style.backgroundColor = '#181818'
@@ -49,7 +52,7 @@ function App() {
 
   AOS.init({
     mirror: false,
-    offset: 200,
+    offset: 100,
     easing: 'ease'
   });
 
@@ -74,7 +77,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App" >
       <video src={video} autoPlay muted loop></video>
       <header ref={Header} className={isActive ? 'card_header' : 'card_header_inactive'}>
         <li onClick={handleChange} className='card_close' style={{ color: 'white' }}>
@@ -114,7 +117,7 @@ function App() {
       </div>
       <div className='card_aboutMe' id='aboutMe' >
         <h2 style={{ color: 'white', textAlign: 'center', margin: '0' }}>About me</h2>
-        <div id='particles-js' data-aos="flip-left" data-aos-duration="2000" data-aos-once="false" data-aos-mirror="true" className='card_personal'>
+        <div data-aos="flip-left" data-aos-duration="2000" data-aos-once="false" data-aos-mirror="true" className='card_personal'>
           <div>
             <img className='card_image' src={image} alt="" />
             <p style={{ color: 'rgb(58, 54, 54)', textAlign: 'center' }}>Hi , my name is Luis .I'm a industrial engineer , interested in the finantial markets and development webs , I'm 24 years old , I'm a resilient and displined person ,willing and focused on fulfilling all the objectives that proposed to me.
@@ -165,7 +168,7 @@ function App() {
                     <div className='card_subida'>
                       <h2 style={{ textAlign: 'center' }} >Pokedex
                       </h2>
-                      <p style={{ color: 'red' }}>Web page focus on searching pokemons by type and name </p>
+                      <p style={{ color: 'red' }}>Web page focus on searching pokemons by type and name , made with Html , CSS , React and the libraries Redux , Routes and Axios  </p>
                       <div style={{ display: 'flex', gap: '8%' }}>
                         <a href="https://fluffy-tarsier-d169ec.netlify.app/#/pokedex" target={'_blank'}><button>Demo</button></a>
                         <a href="https://github.com/LuisJavier98/Pokedex" target={'_blank'}> <button>Code</button></a>
@@ -174,7 +177,9 @@ function App() {
                     : indice == 1 ?
                       <div className='card_subida'>
                         <h2 style={{ textAlign: 'center' }} >E-commerce</h2>
-                        <p style={{ color: 'red' }}></p>
+                        {language == 'english' ?
+                          <p style={{ color: 'black' }}>Web focus on searching products and filtering by price and category , made with Html , CSS , React and the libraries Redux , Routes , Hook form and Axios </p> : <p style={{ color: 'black' }}>Web enfocada a la busqueda de productos de una tienda virtual con opcion a filtrarlo por categoria y precio , hecha con Html , CSS , React and the libraries Redux , Routes , Hook form and Axios </p>
+                        }
                         <div style={{ display: 'flex', gap: '8%' }}>
                           <a href="https://jade-caramel-402266.netlify.app" target={'_blank'}><button>Demo</button></a>
                           <a href="https://github.com/LuisJavier98/Entregable-6" target={'_blank'}><button>Code</button></a>
@@ -182,8 +187,8 @@ function App() {
                       </div>
                       : indice == 2 ?
                         <div className='card_subida'>
-                          <h2 style={{ textAlign: 'center' }} >Rick and Morty Api</h2>
-                          <p style={{ color: 'red' }}></p>
+                          <h2 style={{ textAlign: 'center', color: 'white' }} >Rick and Morty Api</h2>
+                          <p style={{ color: 'white' }}>App focus on searching Rick and Morty's chapters made with Html,CSS, React and the libraries React Select and Axios  </p>
                           <div style={{ display: 'flex', gap: '8%' }}>
                             <a href="https://harmonious-puffpuff-fe0efd.netlify.app" target={'_blank'}><button>Demo</button></a>
                             <a href="https://github.com/LuisJavier98/Entregable-3" target={'_blank'}><button>Code</button></a>
@@ -215,7 +220,7 @@ function App() {
           <label htmlFor="">First Name</label>
           <input type="text" name='first_name' required autoComplete='off' />
           <label htmlFor="">Last Name</label>
-          <input type="text" name='last_name' required autoComplete='off'/>
+          <input type="text" name='last_name' required autoComplete='off' />
           <label htmlFor="">Email</label>
           <input type="email" name='email' required autoComplete='off' />
           <label htmlFor="number">Phone Number</label>
@@ -223,8 +228,6 @@ function App() {
           <label htmlFor="">Message</label>
           <textarea name="message" id="" cols="30" rows="10" required ></textarea>
           <button type='submit' className='button_send'>Send</button>
-
-
         </form>
         <div className='card_icons'>
           <a href="https://www.linkedin.com/in/luis-javier-burga-campos-97bb77186/" target={'_blank'}><AiOutlineLinkedin /></a>
@@ -233,7 +236,75 @@ function App() {
         </div>
         <blockquote style={{ margin: '0px', textAlign: 'center', color: 'white' }}><AiOutlineCopyright /> Copyright 2022</blockquote>
       </footer>
-    </div>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 150,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "rgb(182, 182, 202)",
+            },
+            links: {
+              color: "rgb(182, 182, 202)",
+              distance: 100,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              directions: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: true,
+              speed: 4,
+              straight: true,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 6 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div >
   )
 }
 
