cf1f85f9c7f8098a4c2b7679722db899e935eb35
Luis <luisjavier_2705@hotmail.com>
2023-02-10 09:15:16 -0500

    segundo avance

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,6 @@ import Home from './Project-Components/Home'
 import MyProject from './Project-Components/MyProject'
 import Header from './Project-Components/Header'
 import Footer from './Project-Components/Footer';
-import { BsMoon, BsSun } from 'react-icons/bs'
-import { AiOutlineSetting, AiOutlineClose } from 'react-icons/ai'
-import { flags } from './Projects';
 
 function App() {
 
@@ -28,62 +25,20 @@ function App() {
   const [dark, setdark] = useState(true)
   const [isActive, setisActive] = useState(false)
   const [language, setlanguage] = useState(true)
-  const [deploy, setdeploy] = useState(false)
   const ProjectMove = useRef()
-  let height = window.innerHeight
   const headers = useRef()
   const Reference = useRef()
 
 
-  const handleDeploy = () => {
-    setdeploy(!deploy)
-  }
   const handleDark = () => {
     setdark(!dark)
   }
 
-  window.onscroll = function () {
-    if (dark) {
-      if (scrollY > (height - 10)) {
-        headers.current.style.backgroundColor = '#181818'
-        headers.current.style.backdropFilter = 'blur(0px)'
-        Reference.current.style.backgroundColor = '#181818'
-        Reference.current.style.backdropFilter = 'blur(0px)'
-      }
-      else {
-        headers.current.style.backdropFilter = 'blur(20px)'
-        headers.current.style.backgroundColor = 'transparent'
-        Reference.current.style.backdropFilter = 'blur(20px)'
-        Reference.current.style.backgroundColor = 'transparent'
-      }
-    }
-    else {
-      if (scrollY > (height - 10)) {
-        headers.current.style.backgroundColor = 'rgba(219, 228, 237, 255)'
-        headers.current.style.backdropFilter = 'blur(0px)'
-        Reference.current.style.backgroundColor = 'rgba(219, 228, 237, 255)'
-        Reference.current.style.backdropFilter = 'blur(0px)'
-      }
-      else {
-        headers.current.style.backdropFilter = 'blur(20px)'
-        headers.current.style.backgroundColor = 'transparent'
-        Reference.current.style.backdropFilter = 'blur(20px)'
-        Reference.current.style.backgroundColor = 'transparent'
-      }
-
-    }
-  }
-
-  const buttonDark = document.getElementsByClassName('button_dark')
-  const buttonLanguage = document.getElementsByClassName('button_language')
-  const buttonWhatsapp = document.getElementsByClassName('button_whatsapp')
-
-  const handleChange = () => {
-    setisActive(!isActive)
-  }
-
   const handleLanguage = () => {
     setlanguage(!language)
+  }
+  const handleChange = () => {
+    setisActive(!isActive)
   }
 
   const SelectCard = useRef()
@@ -95,31 +50,15 @@ function App() {
   }
 
 
-  let Childs
   useEffect(() => {
     ProjectMove.current.style.transform = `translateX(${-25 * projectNumber}%)`
     ProjectMove.current.style.transition = 'all 250ms ease'
-    Childs = SelectCard.current.childNodes[0]
-    if (deploy) {
-      buttonDark[0].style.transform = 'translateX(50px)'
-      buttonDark[0].style.transition = 'all 250ms ease'
-      buttonLanguage[0].style.transition = 'all 250ms ease'
-      buttonLanguage[0].style.transform = 'translate(50px, -50px)'
-      buttonWhatsapp[0].style.transform = 'translateY(-50px)'
-      buttonWhatsapp[0].style.transition = 'all 250ms ease'
-    }
-    else {
-      buttonDark[0].style.transform = 'translateX(0px)'
-      buttonDark[0].style.transition = 'all 250ms ease'
-      buttonLanguage[0].style.transform = 'translateX(0px)'
-      buttonLanguage[0].style.transition = 'all 250ms ease'
-      buttonWhatsapp[0].style.transform = 'translateY(0px)'
-      buttonWhatsapp[0].style.transition = 'all 250ms ease'
-    }
   }
-    , [render, isActive, language, dark, deploy])
+    , [render])
+
 
   const ElementS = (altitude, i) => {
+    let Childs = SelectCard.current.childNodes[0]
     Childs.childNodes[i].childNodes[1].style.height = `${altitude}%`
 
   }
@@ -127,18 +66,12 @@ function App() {
 
   return (
     <div className="App" >
-      <button style={{ backgroundImage: language ? `url(${flags[0]})` : `url(${flags[1]})` }} className='button_language' onClick={handleLanguage}></button>
-      <button style={{ backgroundColor: dark ? 'rgb(221, 215, 215)' : 'rgb(62, 59, 59)', color: dark ? 'black' : 'white' }} className='button_dark' onClick={handleDark}>{dark ? <BsSun /> : <BsMoon />}</button>
-      <a href="https://api.whatsapp.com/send?phone=51957580094&text=Hola, Nececito mas informacion!" target='_blank'>
-        <button className='button_whatsapp' style={{ backgroundImage: 'url(https://cdn-icons-png.flaticon.com/512/124/124034.png?w=360)' }} >
-        </button>
-      </a>
-      <button className='button_config' onClick={handleDeploy} style={{ translate: 'all 500ms ease', backgroundColor: dark ? '#181818' : 'rgba(219, 228, 237, 255)', color: dark ? 'white' : 'black' }}> {deploy ? <AiOutlineClose /> : <AiOutlineSetting />}</button>
+
       {
         dark ?
-          <video style={{ transition: 'all 250ms ease' }} src={video} autoPlay muted loop></video>
+          <video src={video} preload autoPlay loop ></video>
           :
-          <video style={{ transition: 'all 250ms ease' }} src={videoLight} autoPlay muted loop></video>
+          <video src={videoLight} preload autoPlay loop ></video>
       }
       <Header
         language={language}
@@ -146,12 +79,14 @@ function App() {
         handleChange={handleChange}
         headers={headers}
         dark={dark}
+        handleDark={handleDark}
+        handleLanguage={handleLanguage}
+
       />
 
       <Home
         language={language}
         handleChange={handleChange}
-        height={height}
         Reference={Reference}
         dark={dark}
       />
--- src/Project-Components/Header.jsx
+++ src/Project-Components/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { BsMoon, BsSun } from 'react-icons/bs'
 import { IoIosCloseCircleOutline } from 'react-icons/io'
+import { flags } from '../Projects'
 
 
 
-const Header = ({ language, isActive, handleChange, headers, dark }) => {
+const Header = ({ language, isActive, handleChange, headers, dark, handleLanguage, handleDark }) => {
 
 
   return (
-    <header ref={headers} className={isActive ? 'card_header' : 'card_header_inactive'} style={{ backgroundColor: dark ? '#181818 ' : 'rgba(219, 228, 237, 255)' }}>
+    <header ref={headers} className={isActive ? 'card_header' : 'card_header_inactive'} style={{ backgroundColor: dark ? 'rgb(0,0,0,0.9)' : 'rgba(219, 228, 237,0.9)' }}>
+      <li className='buttons'>
+        <button style={{ backgroundImage: language ? `url(${flags[0]})` : `url(${flags[1]})` }} className='button_language' onClick={handleLanguage}></button>
+        <button style={{ backgroundColor: dark ? 'rgb(221, 215, 215)' : 'rgb(62, 59, 59)', color: dark ? 'black' : 'white' }} className='button_dark' onClick={handleDark}>{dark ? <BsSun /> : <BsMoon />}</button>
+      </li>
       <li onClick={handleChange} className='card_close' style={{ color: dark ? 'white' : "black", transition: 'all 500ms ease' }}>
         <IoIosCloseCircleOutline />
       </li>
