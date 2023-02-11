3e134573bcf72285b992452736ce9056f1ddd080
Luis <luisjavier_2705@hotmail.com>
2023-02-11 09:53:10 -0500

    quinto avance

--- src/Project-Components/MyProject.jsx
+++ src/Project-Components/MyProject.jsx
@@ -1,60 +1,20 @@
 import React from 'react'
-import Projects from '../Projects.jsx'
+import Projects, { Etiquetas } from '../Projects.jsx'
 
 
 const MyProject = ({ language, ProjectMove, ProjectNext, SelectCard, projectNumber, ElementS, dark }) => {
-
-
   return (
     <div className={dark ? 'card_projects' : 'card_projectsLight'} id='projects'>
       <div data-aos="fade-up" className='card_contain_projects'>
         <div ref={SelectCard} className='card_contain' >
           <div ref={ProjectMove}
             className='projects'>
-            {Projects.map((project, indice) => <div key={indice} onMouseOver={() => ElementS(100, indice)} onMouseOut={() => { ElementS(0, indice) }} className=' project'>
-              <img id={indice} src={project} alt="" />
-              {
-                indice == 0 ?
-                  <div className='card_subida'>
-                    <h2 style={{ textAlign: 'center', color: 'white' }} >Pokedex
-                    </h2>
-                    <p style={{ color: 'white' }}>{language ? 'Web page focus on searching pokemons by type and name , made with Html , CSS , React and the libraries Redux , Routes and Axios' : 'Pagina enfoacada en la busqueda de pokemones por tipo y nombre , desarrolada con Html,CSS,React y sus librerias Routes , Redux y Axios'}  </p>
-                    <div style={{ display: 'flex', gap: '8%' }}>
-                      <a href="https://fluffy-tarsier-d169ec.netlify.app/#/pokedex" target={'_blank'}><button>Demo</button></a>
-                      <a href="https://github.com/LuisJavier98/Pokedex" target={'_blank'}> <button>{language ? 'Code' : 'Codigo'}</button></a>
-                    </div>
-                  </div>
-                  : indice == 1 ?
-                    <div className='card_subida'>
-                      <h2 style={{ textAlign: 'center', color: 'white' }} >E-commerce</h2>
-                      <p style={{ color: 'white' }}>{language ? 'Web focus on searching products and filtering by price and category , developed with Html , CSS , React and the libraries Redux , Routes , Hook form and Axios' : 'Web enfocada a la busqueda de productos de una tienda virtual con opcion a filtrarlo por categoria y precio , desarrollada con Html , CSS , React and the libraries Redux , Routes , Hook form and Axios'} </p>
-
-                      <div style={{ display: 'flex', gap: '8%' }}>
-                        <a href="https://jade-caramel-402266.netlify.app" target={'_blank'}><button>Demo</button></a>
-                        <a href="https://github.com/LuisJavier98/Entregable-6" target={'_blank'}><button>{language ? 'Code' : 'Codigo'}</button></a>
-                      </div>
-                    </div>
-                    : indice == 2 ?
-                      <div className='card_subida'>
-                        <h2 style={{ textAlign: 'center', color: 'white' }} >Rick and Morty Api</h2>
-                        <p style={{ color: 'white' }}>{language ? 'App focus on searching Rick and Mortys chapters made with Html,CSS, React and the libraries React Select and Axios' : 'Web enfocada en la busqueda de capitulos de la serie Rick and Morty, desarrollada con Html,CSS,React y las librerias React Select y Axios '}  </p>
-                        <div style={{ display: 'flex', gap: '8%' }}>
-                          <a href="https://harmonious-puffpuff-fe0efd.netlify.app" target={'_blank'}><button>Demo</button></a>
-                          <a href="https://github.com/LuisJavier98/Entregable-3" target={'_blank'}><button>{language ? 'Code' : 'Codigo'}</button></a>
-                        </div>
-                      </div>
-                      : <div className='card_subida'>
-                        <h2 style={{ textAlign: 'center', color: 'white' }} >Weather Api</h2>
-                        <p style={{ color: 'white' }}>{
-                          language ? 'Web focus on showing the dates about the weather in the countrey where was searching ,it was developed with Html,CSS , React and its library Axios' : 'Web enfocada en mostras los datos acerca del clima en el lugar donde es requerida , fue realizada con Html , CSS ,React y su libreria Axios'
-                        }</p>
-                        <div style={{ display: 'flex', gap: '8%' }}>
-                          <a href="https://golden-licorice-3c5c45.netlify.app" target={'_blank'}><button>Demo</button></a>
-                          <a href="https://github.com/LuisJavier98/Segundo-entrgable" target={'_blank'}><button>{language ? 'Code' : 'Codigo'}</button></a>
-                        </div>
-                      </div>
-              }
-            </div>)}
+            {Projects.map((project, indice) => (
+              <div key={indice} onMouseOver={() => ElementS(100, indice)} onMouseOut={() => { ElementS(0, indice) }} className=' project'>
+                <img id={indice} src={project} alt="" />
+                {Etiquetas(language, indice)}
+              </div>
+            ))}
           </div>
         </div>
         <div className='card_contain_buttonProject'>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -27,3 +27,75 @@ export const skills = [<ImHtmlFive style={{ width: '80%', height: '80%', filter:
 <FaNodeJs style={{ width: '80%', height: '80%', filter: `drop-shadow(${backdrop})` }} />
 ]
 export const flags = [spain, england]
+
+export function Etiquetas(language, index) {
+  const etiquetas = {
+    0: <div className='card_subida'>
+      <h2 style={{ textAlign: 'center', color: 'white' }} >Pokedex
+      </h2>
+      <p style={{ color: 'white' }}>{language ? 'Web page focus on searching pokemons by type and name , made with Html , CSS , React and the libraries Redux , Routes and Axios' : 'Pagina enfoacada en la busqueda de pokemones por tipo y nombre , desarrolada con Html,CSS,React y sus librerias Routes , Redux y Axios'}  </p>
+      <div style={{ display: 'flex', gap: '8%' }}>
+        <a href="https://fluffy-tarsier-d169ec.netlify.app/#/pokedex" target={'_blank'}><button>Demo</button></a>
+        <a href="https://github.com/LuisJavier98/Pokedex" target={'_blank'}> <button>{language ? 'Code' : 'Codigo'}</button></a>
+      </div>
+    </div>,
+    1: <div className='card_subida'>
+      <h2 style={{ textAlign: 'center', color: 'white' }} >E-commerce</h2>
+      <p style={{ color: 'white' }}>{language ? 'Web focus on searching products and filtering by price and category , developed with Html , CSS , React and the libraries Redux , Routes , Hook form and Axios' : 'Web enfocada a la busqueda de productos de una tienda virtual con opcion a filtrarlo por categoria y precio , desarrollada con Html , CSS , React and the libraries Redux , Routes , Hook form and Axios'} </p>
+      <div style={{ display: 'flex', gap: '8%' }}>
+        <a href="https://jade-caramel-402266.netlify.app" target={'_blank'}><button>Demo</button></a>
+        <a href="https://github.com/LuisJavier98/Entregable-6" target={'_blank'}><button>{language ? 'Code' : 'Codigo'}</button></a>
+      </div>
+    </div>,
+    2: <div className='card_subida'>
+      <h2 style={{ textAlign: 'center', color: 'white' }} >Rick and Morty Api</h2>
+      <p style={{ color: 'white' }}>{language ? 'App focus on searching Rick and Mortys chapters made with Html,CSS, React and the libraries React Select and Axios' : 'Web enfocada en la busqueda de capitulos de la serie Rick and Morty, desarrollada con Html,CSS,React y las librerias React Select y Axios '}  </p>
+      <div style={{ display: 'flex', gap: '8%' }}>
+        <a href="https://harmonious-puffpuff-fe0efd.netlify.app" target={'_blank'}><button>Demo</button></a>
+        <a href="https://github.com/LuisJavier98/Entregable-3" target={'_blank'}><button>{language ? 'Code' : 'Codigo'}</button></a>
+      </div>
+    </div>,
+    3: <div className='card_subida'>
+      <h2 style={{ textAlign: 'center', color: 'white' }} >Weather Api</h2>
+      <p style={{ color: 'white' }}>{
+        language ? 'Web focus on showing the dates about the weather in the countrey where was searching ,it was developed with Html,CSS , React and its library Axios' : 'Web enfocada en mostras los datos acerca del clima en el lugar donde es requerida , fue realizada con Html , CSS ,React y su libreria Axios'
+      }</p>
+      <div style={{ display: 'flex', gap: '8%' }}>
+        <a href="https://golden-licorice-3c5c45.netlify.app" target={'_blank'}><button>Demo</button></a>
+        <a href="https://github.com/LuisJavier98/Segundo-entrgable" target={'_blank'}><button>{language ? 'Code' : 'Codigo'}</button></a>
+      </div>
+    </div>,
+    4: <div className='card_subida'>
+      <h2 style={{ textAlign: 'center', color: 'white' }} >Guitar LA</h2>
+      <p style={{ color: 'white' }}>{
+        language ? 'Web focus on showing guitars on sell ,with a data base created in Strapi and publicated in Render,it was developed with TailwindCss and Nextjs ' : 'Web enfocada en mostrar guitarras en venta , la base de datos fue creada en Strapi y publicada en Render , la pagina fue desarrollada con Nextjs y TailwindCss para el diseño '
+      }</p>
+      <div style={{ display: 'flex', gap: '8%' }}>
+        <a href="https://guitarla-ecommerce.netlify.app" target={'_blank'}><button>Demo</button></a>
+        <a href="https://github.com/LuisJavier98/GuitarLA" target={'_blank'}><button>{language ? 'Code' : 'Codigo'}</button></a>
+      </div>
+    </div>,
+    5: <div className='card_subida'>
+      <h2 style={{ textAlign: 'center', color: 'white' }} >Cotizador de Criptomonedas</h2>
+      <p style={{ color: 'white' }}>{
+        language ? 'Web focused on showing the most valued cryptocurrencies in the market, Axios was used for the requests as well as React as the main framework and TailwindCss for the design of the page' : 'Web enfocada en mostrar las crritomonedas mas cotizadas del mercado , se hizo uso de axios para las peticiones asi como React como framework principal y TailwindCss para el diseño de la pagina '
+      }</p>
+      <div style={{ display: 'flex', gap: '8%' }}>
+        <a href="https://rococo-meerkat-e3c46c.netlify.app" target={'_blank'}><button>Demo</button></a>
+        <a href="https://github.com/LuisJavier98/CotizadorDeCryptos" target={'_blank'}><button>{language ? 'Code' : 'Codigo'}</button></a>
+      </div>
+    </div>,
+    6: <div className='card_subida'>
+      <h2 style={{ textAlign: 'center', color: 'white' }} >Planificador de Gastos</h2>
+      <p style={{ color: 'white' }}>{
+        language ? 'Web page designed for budget control, it was designed with React as the main framework and Css for the design, likewise the ReactCircularProgressBar and reactSwipeableList libraries were used to add animations.' : 'Pagina web diseñada para el control presupuestario , fue diseñada con React como principal framework y Css para el diseño , asi mismo se utilizaron las librerias ReactCircularProgressBar y reactSwipeableList para añadirle animaciones.'
+      }</p>
+      <div style={{ display: 'flex', gap: '8%' }}>
+        <a href="https://gleaming-tapioca-c1db7d.netlify.app" target={'_blank'}><button>Demo</button></a>
+        <a href="https://github.com/LuisJavier98/ControlDeGastos" target={'_blank'}><button>{language ? 'Code' : 'Codigo'}</button></a>
+      </div>
+    </div>
+  }
+
+  return etiquetas[index]
+}
